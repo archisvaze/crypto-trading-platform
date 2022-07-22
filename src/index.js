547e07e9c9b9a10a94bdbfdecc027e98dd662b71
archis <vazearchis@gmail.com>
2022-07-22 12:52:23 +0530

    fixed npm start error

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import "./style.css"
 import App from "./components/App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(
-    <App />
-)
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);

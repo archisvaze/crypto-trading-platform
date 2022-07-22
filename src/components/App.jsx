ceeb9f7aa3db5053aa18cfa612c9e5e1594538dd
razen7 <hopeful.raz@gmail.com>
2022-07-22 14:31:13 +0545

    nothing

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,9 @@ import Portfolio from "./Portfolio";
 import Wallet from "./Wallet";
 
 function App(props) {
+    const [bitcoinAmt, setbitcoinAmt] = useState(0);
+    // const [bitcoinAmt, setbitcoinAmt] = useState(0);
+    // const [bitcoinAmt, setbitcoinAmt] = useState(0);
     return (
         <div className="container">
             <Header />

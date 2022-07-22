a66af7a8ae795b999260f5ef52d06837f3b78190
razen7 <hopeful.raz@gmail.com>
2022-07-22 13:25:40 +0545

    portfolio component added

---
+++ src/components/Portfolio.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function Portfolio() {
+    let portfolioAmt = 50.80;
+    return (
+        <div id='portfolio'>Portfolio Value: ${portfolioAmt}</div>
+    )
+}

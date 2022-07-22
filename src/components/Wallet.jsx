e78ba0dda62a841b8495652b6c6c0b0a53d0c9fe
archis <vazearchis@gmail.com>
2022-07-22 13:09:11 +0530

    fixed classNames

--- src/components/Wallet.jsx
+++ src/components/Wallet.jsx
@@ -3,6 +3,6 @@ import React from 'react'
 export default function Wallet() {
     let walletAmount = 49.11;
   return (
-    <div id='wallet-title'>💵 Wallet: ${walletAmount}</div>
+    <div id='wallet'>💵 Wallet: ${walletAmount}</div>
   )
 }

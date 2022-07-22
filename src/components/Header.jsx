c78c780bc98ac0aa41720230bbc275df15bf96b3
razen7 <hopeful.raz@gmail.com>
2022-07-22 13:00:15 +0545

    header initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Header() {
+    return (
+        <header>
+            <h2>
+                Earn some virtual money 💰 
+            </h2>
+            <p>
+                To buy virtual food 🍕
+            </p>
+        </header>
+    )
+}

219528030907d19187989e0b5f69d65d297b320c
archis <vazearchis@gmail.com>
2022-07-22 14:19:01 +0530

    fixed Price

--- src/components/BitcoinCard.jsx
+++ src/components/BitcoinCard.jsx
@@ -2,16 +2,16 @@ import React from 'react'
 import Bitcoin from "../bitcoin.png"
 
 export default function BitcoinCard() {
-    let currAmount = 49128;
-    let prevAmount = 49150;
+    let currPrice = 49128;
+    let prevPrice = 49150;
 
-    let change = (((currAmount - prevAmount) / prevAmount) * 100).toFixed(5);
+    let change = (((currPrice - prevPrice) / currPrice) * 100).toFixed(5);
 
     return (
         <div className="crypto-card">
             <img width={"50px"} height={"50px"} src={Bitcoin} alt="" />
             <div className="crypto-info">
-                <p className="curr-amount">${currAmount}</p>
+                <p className="curr-amount">${currPrice}</p>
                 <p className="name">Bitcoin</p>
                 <p className="change">Last 24h: <span style={{color : change >=0 ? "green" : "red"}}className='percentage'>{change}</span></p>
             </div>
